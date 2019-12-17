4eb88f2486684a2d908b590c5373dafc7f7ee652
Nabarun Pal <pal.nabarun95@gmail.com>
2019-12-18 02:51:49 +0530

    [tf] Moves dns.tf to dns-xyz-nbrn.tf

---
+++ terraform/dns-xyz-nbrn.tf
@@ -0,0 +1,27 @@
+resource "digitalocean_record" "alpha" {
+  domain = "nbrn.xyz"
+  type   = "A"
+  name   = "alpha"
+  value  = "${google_compute_instance.alpha.network_interface.0.access_config.0.nat_ip}"
+}
+
+resource "digitalocean_record" "dc-lb" {
+  domain = "nbrn.xyz"
+  type   = "A"
+  name   = "dc-lb"
+  value  = "35.200.215.211"
+}
+
+resource "digitalocean_record" "dc-root" {
+  domain = "nbrn.xyz"
+  type   = "CNAME"
+  name   = "dc"
+  value  = "dc-lb.nbrn.xyz."
+}
+
+resource "digitalocean_record" "dc-wildcard" {
+  domain = "nbrn.xyz"
+  type   = "CNAME"
+  name   = "*.dc"
+  value  = "dc-lb.nbrn.xyz."
+}
--- terraform/dns.tf
+++
@@ -1,6 +0,0 @@
-resource "digitalocean_record" "alpha" {
-  domain = "nbrn.xyz"
-  type   = "A"
-  name   = "alpha"
-  value  = "${google_compute_instance.alpha.network_interface.0.access_config.0.nat_ip}"
-}
