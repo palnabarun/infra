1562447dfbaf80bab86d3d6b060cc5013d5005dc
Nabarun Pal <pal.nabarun95@gmail.com>
2020-01-23 00:43:58 +0530

    [tf] Remove GCP as a provider

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,9 +1,3 @@
-provider "google" {
-  project     = "experiments-220510"
-  # region      = "us-central1"
-  # zone        = "us-central1-c"
-}
-
 provider "digitalocean" {
   token = "${var.DO_TOKEN}"
 }
