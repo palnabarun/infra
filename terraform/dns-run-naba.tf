c9909ee3ba53601fa609a6d603d8211665ac9c02
Nabarun Pal <pal.nabarun95@gmail.com>
2020-01-23 00:32:28 +0530

    [tf] Cleanup naba.run domain space

--- terraform/dns-run-naba.tf
+++ terraform/dns-run-naba.tf
@@ -1,10 +1,3 @@
 resource "digitalocean_domain" "run-naba" {
   name = "naba.run"
 }
-
-resource "digitalocean_record" "apps-bank" {
-  domain = "naba.run"
-  type   = "CNAME"
-  name   = "bank.apps"
-  value  = "apps.nbrn.xyz."
-}
