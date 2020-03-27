898018095fa551dcb774aa0ccc80b381159b195f
Nabarun Pal <pal.nabarun95@gmail.com>
2020-03-28 02:04:15 +0530

    [tf:do-npal] Modify ssh public key to the one in the repository

--- terraform/do-npal/keys.tf
+++ terraform/do-npal/keys.tf
@@ -1,4 +1,4 @@
 resource "digitalocean_ssh_key" "nabarun_pal" {
     name = "nabarun_pal"
-    public_key = file("/home/nabarun/.ssh/id_rsa.pub")
+    public_key = file("../../ansible/authorized_keys/nabarun_id_rsa.pub")
 }
