954011ec8a4bf6d53d27f33123e349c96bc121b2
Nabarun Pal <pal.nabarun95@gmail.com>
2020-03-27 16:37:28 +0530

    [tf:do-palnabarun] Add machine IP's

--- terraform/do-palnabarun/dns.tf
+++ terraform/do-palnabarun/dns.tf
@@ -1,6 +1,35 @@
 resource "digitalocean_domain" "run_naba" {
   name = "naba.run"
 }
+
 resource "digitalocean_domain" "xyz_nbrn" {
   name = "nbrn.xyz"
+}
+
+resource "digitalocean_record" "iris" {
+  domain = digitalocean_domain.xyz_nbrn.name
+  name = "iris"
+  type = "A"
+  value = var.IRIS_FIP
+}
+
+resource "digitalocean_record" "iris_vpn" {
+  domain = digitalocean_domain.xyz_nbrn.name
+  name = "vpn.iris"
+  type = "CNAME"
+  value = "${digitalocean_record.iris.fqdn}."
+}
+
+resource "digitalocean_record" "vpn" {
+  domain = digitalocean_domain.xyz_nbrn.name
+  name = "vpn"
+  type = "CNAME"
+  value = "${digitalocean_record.iris.fqdn}."
+}
+
+resource "digitalocean_record" "jarvis" {
+  domain = digitalocean_domain.xyz_nbrn.name
+  name = "jarvis"
+  type = "A"
+  value = var.JARVIS_FIP
 }
