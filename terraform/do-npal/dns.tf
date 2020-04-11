337300f859faf3e3778f45734a18e80779c1ccef
Nabarun Pal <pal.nabarun95@gmail.com>
2020-04-11 21:26:54 +0530

    [tf:do-npal] Remove jarvis

--- terraform/do-npal/dns.tf
+++ terraform/do-npal/dns.tf
@@ -8,10 +8,3 @@ resource "digitalocean_record" "iris_m_nabarun_in" {
   type = "A"
   value = digitalocean_floating_ip.iris_fip.ip_address
 }
-
-resource "digitalocean_record" "jarvis_m_nabarun_in" {
-  domain = digitalocean_domain.m_nabarun_in.name
-  name = "jarvis"
-  type = "A"
-  value = digitalocean_floating_ip.jarvis_fip.ip_address
-}
