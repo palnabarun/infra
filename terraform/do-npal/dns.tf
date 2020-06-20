59bf886b619bcb330997620f0662354cff31a0ed
Nabarun Pal <pal.nabarun95@gmail.com>
2020-06-20 18:27:46 +0530

    [tf:do-npal] Adds alpha in BLR
    Signed-off-by: Nabarun Pal <pal.nabarun95@gmail.com>

--- terraform/do-npal/dns.tf
+++ terraform/do-npal/dns.tf
@@ -2,9 +2,9 @@ resource "digitalocean_domain" "m_nabarun_in" {
   name = "m.nabarun.in"
 }
 
-resource "digitalocean_record" "prow_m_nabarun_in" {
+resource "digitalocean_record" "alpha_m_nabarun_in" {
   domain = digitalocean_domain.m_nabarun_in.name
-  name = "prow"
+  name = "alpha"
   type = "A"
-  value = digitalocean_floating_ip.prow_fip.ip_address
+  value = digitalocean_floating_ip.alpha_fip.ip_address
 }
--- terraform/do-npal/droplets.tf
+++ terraform/do-npal/droplets.tf
@@ -1,16 +1,16 @@
-resource "digitalocean_droplet" "prow" {
-  image  = "ubuntu-18-04-x64"
-  name   = "prow"
+resource "digitalocean_droplet" "alpha" {
+  image  = "ubuntu-20-04-x64"
+  name   = "alpha"
   region = "blr1"
   size   = "s-1vcpu-1gb"
 
   monitoring = true
   ipv6 = true
   ssh_keys = [digitalocean_ssh_key.nabarun_pal.id]
-  tags = ["prow", "blr"]
+  tags = ["alpha", "blr"]
 }
 
-resource "digitalocean_floating_ip" "prow_fip" {
-    droplet_id = digitalocean_droplet.prow.id
-    region = digitalocean_droplet.prow.region
+resource "digitalocean_floating_ip" "alpha_fip" {
+    droplet_id = digitalocean_droplet.alpha.id
+    region = digitalocean_droplet.alpha.region
 }
